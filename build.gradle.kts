b9d71762f1c8b210eb5b617efca8a092f04d6dfb
Jiandong Ma <jiandong.ma.cn@gmail.com>
2025-10-21 22:41:15 +0800

    Restructure to support multi-projects build. (#3)
    I just realized for spring cloud projects, it's hard also not correct to integrate everything in a single project.
    
    like gateway should be gateway only, should not integrate config-server in it. vice versa.
    
    change ci `./gradlew build` to `./gradlew check`.
    
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	id("org.springframework.boot") version "4.0.0-M3"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "4.0.0-M3"
+    id("io.spring.dependency-management") version "1.1.7"
     jacoco
 }
 
@@ -9,46 +9,58 @@ group = "com.jiandong"
 version = "0.0.1-SNAPSHOT"
 description = "spring-cloud-unknown"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
-extra["springCloudVersion"] = "2025.1.0-M3"
-extra["wireMockSpringBoot"] = "3.10.0"
-
-dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.wiremock.integrations:wiremock-spring-boot:${property("wireMockSpringBoot")}")
-    testImplementation("org.eclipse.jetty.ee10:jetty-ee10-bom:12.1.3") // https://github.com/wiremock/wiremock-spring-boot/issues/137
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
-}
-
-tasks.withType<Test> {
-
-    testLogging.showStandardStreams = true
-
-    useJUnitPlatform()
-
-    finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.required = false
-        csv.required = true
-        html.required = false
+allprojects {
+    repositories {
+        mavenCentral()
     }
 }
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "jacoco")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    extra["springCloudVersion"] = "2025.1.0-M3"
+
+    dependencies {
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+        }
+    }
+
+    tasks.withType<Test> {
+
+        testLogging.showStandardStreams = true
+
+        systemProperty(
+            "junit.jupiter.testclass.order.default",
+            "org.junit.jupiter.api.ClassOrderer\$OrderAnnotation"
+        )
+
+        useJUnitPlatform()
+
+        finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
+    }
+
+    tasks.jacocoTestReport {
+        reports {
+            xml.required = false
+            csv.required = true
+            html.required = false
+        }
+    }
+
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "spring-cloud-unknown"
+
+include("gateway")
