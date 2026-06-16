e42e51e0d944d645558a63ec2ce931f8692d924c
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-06-16 10:08:33 +0800

    delete unused property
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,6 @@ subprojects {
         }
     }
 
-    extra["springCloudVersion"] = "2025.1.0-M4"
-
     dependencies {
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
